10d0d1a99819a2e006774c6a0660c29759591115
Angel Daniel Aviles cruz <angeldaniel.avilescruz66@gmail.com>
2021-03-01 12:59:09 -0600

    primer selector de regiones

--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -5,5 +5,11 @@ import { Injectable } from '@angular/core';
 })
 export class PaisesService {
 
+  private _regiones : string[] = [ 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+  get regiones(){
+    return [...this._regiones]; // se envia el arreglo destructurado para evitar modificar accidentalmente el arreglo principal
+  }
+
   constructor() { }
 }
