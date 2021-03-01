f9d17994edf109980cc9566cb308ed4523f7fe95
Angel Daniel Aviles cruz <angeldaniel.avilescruz66@gmail.com>
2021-03-01 13:43:56 -0600

    segundo selector anidado a la region

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { switchMap, tap } from "rxjs/operators";
+
 import { PaisesService } from '../../services/paises.service';
+import { PaisSmall } from '../../interfaces/paises.interface';
 
 @Component({
   selector: 'app-selector-page',
@@ -11,11 +14,13 @@ import { PaisesService } from '../../services/paises.service';
 export class SelectorPageComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
-    region: ['', Validators.required]
+    region: ['', Validators.required],
+    pais: ['', Validators.required]
   })
 
   // llenar selectores
   regiones: string[] = [];
+  paises : PaisSmall[] = [];
 
   constructor(private fb: FormBuilder,
               private paisService: PaisesService ) { }
@@ -23,6 +28,33 @@ export class SelectorPageComponent implements OnInit {
   ngOnInit(): void {
 
     this.regiones = this.paisService.regiones;
+
+    //cuando cambie la region
+    // this.miFormulario.get('region')?.valueChanges
+    // .subscribe( region =>{
+    //   console.log(region);
+
+    //   this.paisService.getPaisesPorRegion( region )
+    //   .subscribe(paises =>{
+    //     console.log(paises);
+        
+    //       this.paises = paises          
+    //   })
+      
+    // })
+
+    this.miFormulario.get('region')?.valueChanges
+    .pipe(
+      tap( ( _ ) => {
+        this.miFormulario.get('pais')?.reset('');
+      }),
+      switchMap(region => this.paisService.getPaisesPorRegion( region ))
+    )
+    .subscribe( paises => {
+      console.log(paises)
+      this.paises = paises   
+      
+    })
 
   }
 
